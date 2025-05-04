5e94c766e15734a351b42c59283a4e07e997c97f
Szapt <129530552+Szapt@users.noreply.github.com>
2025-05-04 10:36:39 -0500

    initial commit

---
+++ 1066270657-reto2/prueba-lis/src/main/java/com/prueba/lis/PruebaLisApplication.java
@@ -0,0 +1,13 @@
+package com.prueba.lis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PruebaLisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PruebaLisApplication.class, args);
+	}
+
+}
---
+++ 1066270657-reto2/prueba-lis/src/test/java/com/prueba/lis/PruebaLisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prueba.lis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PruebaLisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
